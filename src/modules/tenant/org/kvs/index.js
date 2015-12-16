0dc4b3377bf820336caad09cdb2ff61ec2a848a3
henryleu <henryleu@126.com>
2015-12-16 11:19:38 +0800

    lower case tenantWechatBot

--- src/modules/tenant/org/kvs/index.js
+++ src/modules/tenant/org/kvs/index.js
@@ -3,6 +3,6 @@ var u = require('../../../../app/util');
 var context = require('../../../../context/context');
 
 var TenantWechatBot = require('./TenantWechatBot');
-module.exports.TenantWechatBot = Promise.promisifyAll(new TenantWechatBot(context));
+module.exports.tenantWechatBot = Promise.promisifyAll(new TenantWechatBot(context));
 
 u.extend(context.kvs, module.exports);
