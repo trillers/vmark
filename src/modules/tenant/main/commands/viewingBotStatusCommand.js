3acd25e1e7ca667072da5ce1a8554ca40e978dd4
henryleu <henryleu@126.com>
2015-12-16 21:45:38 +0800

    fix view status command

--- src/modules/tenant/main/commands/viewingBotStatusCommand.js
+++ src/modules/tenant/main/commands/viewingBotStatusCommand.js
@@ -1,14 +1,16 @@
-var context= require('../../../../context/context');
-var wechatMediaService = context.services.wechatMediaService;
-var wechatApi = require('../../../wechat/common/api').api;
-var WechatBotStatus = require('../../../common/models/TypeRegistry').item('WechatBotStatus');
-var inspect = require('util').inspect;
 var co = require('co');
+var inspect = require('util').inspect;
+var WechatBotStatus = require('../../../common/models/TypeRegistry').item('WechatBotStatus');
+var wechatApi = require('../../../wechat/common/api').api;
+var serviceContext = require('../../../../context/context');
+var wechatMediaService = serviceContext.services.wechatMediaService;
+var tenantWechatBotKv = serviceContext.kvs.tenantWechatBot;
 module.exports = function(context){
     var openid = context.weixin.FromUserName;
     co(function*(){
         try{
-            var bot = yield wechatMediaService.findBotByOpenidAsync(openid);
+            var botOpenid = yield tenantWechatBotKv.getBotOpenidAsync(openid);
+            var bot = yield wechatMediaService.findBotByOpenidAsync(botOpenid);
             if(bot){
                 var text = '[系统]: 微信助手号当前状态为 ' + WechatBotStatus.valueNames(bot.status);
                 yield wechatApi.sendTextAsync(openid, text);
