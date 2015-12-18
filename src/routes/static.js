f836965a1f58be1d5d864eb11650c8be5bd7f72c
henryleu <henryleu@126.com>
2015-12-18 16:36:26 +0800

    use mount to static public and web dir

--- src/routes/static.js
+++ src/routes/static.js
@@ -1,6 +1,11 @@
 var staticServer = require('koa-static');
+var mount = require('koa-mount');
 var path = require('path');
+
 module.exports = function(app){
-    app.use(staticServer(path.join(__dirname, '../../public')));
-    app.use(staticServer(path.join(__dirname, '../../web')));
+    //app.use(staticServer(path.join(__dirname, '../../public')));
+    //app.use(staticServer(path.join(__dirname, '../../web')));
+    app.use(mount('/public', staticServer(path.join(__dirname, '../../public'))));
+    app.use(mount('/web', staticServer(path.join(__dirname, '../../web'))));
+
 }
