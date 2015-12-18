68eda8607b1a8133aa1ae24ad0da4417c415cbb1
henryleu <henryleu@126.com>
2015-12-18 20:31:03 +0800

    add mock-login

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,17 +1,15 @@
 var Router = require('koa-router');
 var context = require('../context/context');
-//var authService = context.services.authService;
+//var securityService = context.services.securityService;
 
 module.exports = function(app){
     var router = new Router();
 
-    router.get('/mock-login-list', function *(){
-        //var users = yield authService.loadAllTenantAdminUsersAsync();
-        var users = [{
+    var mockUsers = [{
             "openid": "okvXqs_VftHruzwFV9rx4Pbd_fno",
             "nickname": "小小星星妹",
             "headimgurl": "http://wx.qlogo.cn/mmopen/CyYbk1vmHvYCTpBHH4UiblXdxyuNYN2BX7E8fVw1kUAASwIrlS0nMfMIrsbby4oILcMYic2tCJLJTuhTbngb2hhtTbiaAic3mBFY/0"
-         },
+        },
         {
             "openid": "okvXqswFmgRwEV0YrJ-h5YvKhdUk",
             "nickname": "祺天大剩",
@@ -27,13 +25,26 @@ module.exports = function(app){
             "nickname": "包三哥",
             "headimgurl": "http://wx.qlogo.cn/mmopen/CyYbk1vmHvYCTpBHH4UiblcOM6IEMibm2VweVnbTm5tnWib1rQG5v6t7779AEnDSkFf212MXOVXX29JvZlKicjhUxjpRYDnTPTES/0"
         }
-        ];
-        yield this.render('mock-login-list', {users: users});
+    ];
+    router.get('/mock-login-list', function *(){
+        //var users = yield securityService.loadAllTenantAdminUsersAsync();
+        yield this.render('mock-login-list', {users: mockUsers});
     });
 
     router.get('/mock-login', function *(){
-        //var users = yield authService.loadAllTenantAdminUsersAsync();
-        yield this.render('mock-login');
+        var openid = this.query.openid;
+        if(!openid){
+            yield this.render('mock-login-list', {users: mockUsers});
+            return;
+        }
+        //var auth = yield securityService.authenticateAsync();
+        var auth = mockUsers[0];
+        if(!auth){
+            yield this.render('mock-login');
+            return;
+        }
+        this.session.auth = auth;
+        this.redirect('/');
     });
 
     app.use(router.routes());
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 module.exports = function(app){
     require('./favicon')(app);
     require('./heartbeat')(app);
-    require('./auth')(app);
     require('./static')(app);
     app.keys = ['keys', 'keykeys'];
     app.use(require('../middlewares/session')());
+    require('./auth')(app);
     require('../controllers')(app);
 
 }
