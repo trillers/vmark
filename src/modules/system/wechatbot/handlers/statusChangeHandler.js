eeceec04a9e6040c3842cc934d13272aef8b9831
leekangtaqi <leekangtaqi@sina.com>
2015-12-30 15:08:05 +0800

    debug

--- src/modules/system/wechatbot/handlers/statusChangeHandler.js
+++ src/modules/system/wechatbot/handlers/statusChangeHandler.js
@@ -5,18 +5,21 @@ var co = require('co');
 
 module.exports = function(msg){
     co(function*(){
-        var wechatMediaService = context.services.wechatMediaService;
-        var media = yield wechatMediaService.findBotByOpenidAsync(msg.AgentId);
-        if(media){
-            yield wechatMediaService.updateStatusByIdAsync(media._id, msg.NewStatus);
-            ws.of('/bot/st_change').in(media.org).clients().emit({
-                agent: msg.AgentId,
-                newStatus: msg.NewStatus
-            });
-            logger.info('success update wechat bot status, changeInfo: ' + msg);
-        }else{
-            logger.error('agent status change handler err: no such media, changeInfo: ' + msg);
+        try{
+            var wechatMediaService = context.services.wechatMediaService;
+            var media = yield wechatMediaService.findBotByOpenidAsync(msg.AgentId);
+            if(media){
+                yield wechatMediaService.updateStatusByIdAsync(media._id, msg.NewStatus);
+                ws.of('/bot/st_change').in(media.org).clients().emit({
+                    agent: msg.AgentId,
+                    newStatus: msg.NewStatus
+                });
+                logger.info('success update wechat bot status, changeInfo: ' + msg);
+            }else{
+                logger.error('agent status change handler err: no such media, changeInfo: ' + msg);
+            }
+        }catch(e){
+            console.error(e);
         }
-
     })
 }
