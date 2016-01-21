2ac789311ba4c12217fd788ba9fd417355d8ed59
node-rookie <749924911@qq.com>
2016-01-21 20:25:37 +0800

    merge

--- src/app/qnClient.js
+++ src/app/qnClient.js
@@ -1,5 +1,5 @@
 var qn = require('./qn');
-var Promise = requrie('bluebird');
+var Promise = require('bluebird');
 
 var client = new qn('Ulac7lEuDqiZ593owTOcQ0L8QieCC8D0tm6itYaU', 'PohkfZBuxkAxLiIk6lMR39IdbPy6jpPM27jtEL03', 'china', '7u2kxz.com2.z0.glb.qiniucdn.com');
 
