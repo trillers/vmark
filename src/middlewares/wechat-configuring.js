7be74fad7dd018927fb39c358a6ba14cff272585
henryleu <henryleu@126.com>
2016-01-23 11:10:15 +0800

    update wechat-configuring middleware

--- src/middlewares/wechat-configuring.js
+++ src/middlewares/wechat-configuring.js
@@ -8,13 +8,9 @@ var env = settings.env.NODE_ENV;
 var debug = false;
 
 var wechatConfiguring = function*(next) {
-    var url = this.protocol + '://' + this.get('host') + this.originalUrl;
-    this.app || (req.app = {});
-    this.app.env = env;
-    this.app.debug = debug;
+    var url = this.request.href;
     try {
         var jsConfig = yield token.getJc({url: url, jsApiList: jsApiList, debug: debug});
-        this.app.jc = jsConfig;
         this.state.__page.jc = jsConfig;
     }catch (err){
         logger.error('Fail to get jc: ' + err);
