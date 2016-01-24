ba8df1c82793ba36248ba2cd11954b80374ee057
henryleu <henryleu@126.com>
2016-01-24 13:50:11 +0800

    apply generating auth filters

--- src/controllers/modules/membook.js
+++ src/controllers/modules/membook.js
@@ -6,13 +6,18 @@ var noteServcie = context.services.noteService;
 var TypeRegistry = require('../../modules/common/models/TypeRegistry');
 var NoteType = TypeRegistry.item('NoteType');
 var authFilter = require('../../modules/membook/middlewares/authFilter');
+var generateAuthFilter = require('../../modules/membook/middlewares/generateAuthFilter');
+var needBaseInfoFilter = generateAuthFilter(1);
+//var needUserInfoFilter = generateAuthFilter(2);
+var needSubscriptionFilter = generateAuthFilter(3);
+
 module.exports = function(){
     var router = new Router();
     router.prefix('/note');
     require('../../app/routes-spa')(router);
     //router.use(authFilter); //add auth Filter
 
-    router.get('/new', function *(){
+    router.get('/new', needSubscriptionFilter, function *(){
         try{
             var id = this.session['draftId'];
             if(!id){
@@ -26,7 +31,7 @@ module.exports = function(){
 
     });
 
-    router.get('/_:id', function *(){
+    router.get('/_:id', needBaseInfoFilter, function *(){
         var id = this.params.id;
         yield this.render('note', {id: id});
     });
