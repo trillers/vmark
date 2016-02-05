b24ad24883dc803e680c1a605cb18428fb0cb1aa
leekangtaqi <leekangtaqi@sina.com>
2016-02-05 14:39:47 +0800

    debug

--- src/middlewares/wechat-configuring.js
+++ src/middlewares/wechat-configuring.js
@@ -5,7 +5,7 @@ var token = require('../modules/wechat/common/token');
 var jsApiList = ['checkJsApi','chooseImage','previewImage','uploadImage','downloadImage','onMenuShareTimeline','onMenuShareAppMessage','onMenuShareQQ','onMenuShareWeibo','getNetworkType','startRecord','stopRecord','onVoiceRecordEnd','playVoice','pauseVoice','stopVoice','onVoicePlayEnd','uploadVoice','downloadVoice'];
 
 var env = settings.env.NODE_ENV;
-var debug = false;
+var debug = true;
 
 var wechatConfiguring = function*(next) {
     var url = this.request.href;
