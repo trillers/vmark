73dc0a6b9455bcece935b35d8a835a039f59adb8
leekangtaqi <leekangtaqi@sina.com>
2016-02-14 15:30:02 +0800

    debug

--- src/modules/membook/services/NoteService.js
+++ src/modules/membook/services/NoteService.js
@@ -177,7 +177,8 @@ Service.prototype.filter = function (params, callback) {
             return;
         }
         var docs = rawDocs.map(function(doc){
-            return doc.toObject();
+            console.warn(doc);
+            return doc;
         });
         if (callback) callback(null, docs);
     });
