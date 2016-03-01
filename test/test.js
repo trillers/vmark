d67846942c5d3b44ca3a2a807e3d437752cfb4ee
leekangtaqi <leekangtaqi@sina.com>
2016-03-01 21:03:35 +0800

    ok

--- test/test.js
+++ test/test.js
@@ -1,8 +1,2 @@
-var arr = [1, 2, '11'];
-var lastDate = (new Date()).getTime();
-console.log(arr.some(i=>i===2))
-console.warn((new Date()).getTime() - lastDate)
-
-var lastDate2 = (new Date()).getTime();
-console.log(arr.indexOf(2))
-console.warn((new Date()).getTime() - lastDate2)
+var assert = require('assert');
+console.log({name: '11'} === {name: '11'})
