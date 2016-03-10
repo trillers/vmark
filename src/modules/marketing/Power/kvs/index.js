163df7af2ba1731542c0f9ad370697f9375be404
lvhongli <lvhongli@baijiahulian.com>
2016-03-10 13:49:04 +0800

    solution 2 for optimizing ranking list

--- src/modules/marketing/Power/kvs/index.js
+++
@@ -1,9 +0,0 @@
-var Promise = require('bluebird');
-var u = require('../../../../app/util');
-var context = require('../../../../context/context');
-
-var Power = require('./Power');
-
-module.exports.power = Promise.promisifyAll(new Power(context));
-
-u.extend(context.kvs, module.exports);
