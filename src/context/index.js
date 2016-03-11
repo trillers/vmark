530852d4ac7176684b8c24c96b8f5bbb09887212
node-rookie <749924911@qq.com>
2016-03-11 15:33:44 +0800

    annotation wechatBotManager start

--- src/context/index.js
+++ src/context/index.js
@@ -24,7 +24,7 @@ context.domainBuilder.main = require('../framework/model/DomainBuilder');
 ar.ready(function(){
     require('../modules');
     require('../jobs/syncPowerActivity');
-    context.wechatBotManager.start();
+    //context.wechatBotManager.start();
 });
 
 ar.context = context;
