6e1bc8363dd4dab64f9838830c412dc8329f47fe
node-rookie <749924911@qq.com>
2016-03-21 19:22:25 +0800

    debug

--- src/modules/marketing/power_new/kvs/Power.js
+++ src/modules/marketing/power_new/kvs/Power.js
@@ -98,6 +98,9 @@ Kv.prototype.linkUserToParticipantId = function(activityId, userId, participantI
 Kv.prototype.getParticipantIdByUserIdAndActivityId = function(activityId, userId, callback){
     var redis = this.context.redis.main;
     var key = activityIdToUserIdParticipantIdHashKey(activityId);
+    console.error(activityId);
+    console.error(userId);
+    console.error(key);
     redis.hget(key, userId, function(err, result){
         console.error('*************');
         console.log(result);
