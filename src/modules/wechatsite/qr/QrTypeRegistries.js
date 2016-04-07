4326121264290dcf8e85488a7ab0d4daa0c1f495
node-rookie <749924911@qq.com>
2016-04-07 21:18:48 +0800

    debug

--- src/modules/wechatsite/qr/QrTypeRegistries.js
+++ src/modules/wechatsite/qr/QrTypeRegistries.js
@@ -4,6 +4,7 @@ var Qr = require('./Qr');
 var Persistence = require('./Persistence');
 var tenantPersistence = new Persistence.TenantPersistence();
 var platformPersistence = new Persistence.PlatformPersistence();
+var _ = require('underscore');
 
 function QrTypeRegistry(persistence){
     this.persistence = persistence;
@@ -23,7 +24,7 @@ regProto.newType = function(type, options, listeners){
         throw new Error('Failed to register qr to qr manager input error, first argument must be string or Qr')
     }
     json.registry = this;
-    Object.assign(json, options);
+    _.extend(json, options);
     if(listeners && typeof listeners != 'object'){
         throw new Error('Failed to register qr to qr manager input error, second arguments must be object')
     }
