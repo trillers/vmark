6b384bda02f9dea18dd46b7494a096d576f00123
leekangtaqi <leekangtaqi@sina.com>
2016-04-07 10:48:04 +0800

    add onsubscribe

--- src/modules/tenant/wechat/handlers/eventHandler.js
+++ src/modules/tenant/wechat/handlers/eventHandler.js
@@ -1,25 +1,22 @@
 var co = require('co');
 var logger = require('../../../../app/logging').logger;
 var context = require('../../../../context/context');
-var qrTypeRegistry = require('../../../wechatsite/qr');
+var qrTypeRegistry = require('../../../wechatsite/qr/QrTypeRegistries').tenantQrTypeRegistry;
 
 module.exports = function (emitter) {
     emitter.qr(function (event, ctx) {
         co(function*() {
             var sceneId = ctx.weixin.SceneId;
-            var userOpenid = ctx.weixin.FromUserName;
+            var openid = ctx.weixin.FromUserName;
             var wechatId = ctx.weixin.ToUserName;
-            qrTypeRegistry.handle(sceneId, userOpenid, wechatId);
-            console.log(sceneId);
+            qrTypeRegistry.handle(sceneId, openid, wechatId);
         });
     });
     emitter.subscribe(function (event, ctx){
         co(function*(){
             var openid = ctx.weixin.FromUserName;
             var wechatId = ctx.weixin.ToUserName;
-            var result = yield context.services.tenantAuthenticationService.signupOnSubscriptionAsync(wechatId, openid);
-            console.log('subscribe result..........');
-            console.log(result);
+            yield context.services.tenantAuthenticationService.signupOnSubscriptionAsync(wechatId, openid);
         })
     })
 };
