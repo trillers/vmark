02d669ffaaa7c6fe6c9f4be1da4faf3c107d6996
leekangtaqi <leekangtaqi@sina.com>
2016-04-11 15:34:57 +0800

    ok

--- src/modules/sd/poster/services/PosterService.js
+++ src/modules/sd/poster/services/PosterService.js
@@ -17,7 +17,17 @@ Service.prototype.loadByQrCodeId = function(qrCodeId, callback){
     var posterKv = this.context.kvs.poster;
     var Poster = this.context.models.Poster;
 
-    Poster.findOne({qr: qrCodeId}, callback);
+    Poster.findOne({qr: qrCodeId})
+    .populate({
+        path: 'qr'
+    }).exec(function(err, doc){
+        if(err){
+            return callback(err)
+        }
+        var obj = doc.toObject();
+        obj.ticket = obj.qr.ticket;
+        callback(null, obj);
+    });
 };
 
 Service.prototype.loadByProductIdAndWechatId = function(productId, wechatId, callback){
