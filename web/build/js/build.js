5b698b48695c13021feec4ae8a2bda5448b5565f
node-rookie <749924911@qq.com>
2016-04-14 10:48:18 +0800

    update

--- web/build/js/build.js
+++ web/build/js/build.js
@@ -19,9 +19,9 @@ webpackJsonp([0,1],[
 	var _index2 = __webpack_require__(20);
 
 	function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
-	alert('riot')
-	//__webpack_require__(22);
-	//__webpack_require__(23);
+
+	__webpack_require__(22);
+	__webpack_require__(23);
 
 	window.app = _app.app;
 	window.actions = _index.actions;
