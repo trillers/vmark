936a0a4eca1ef2e7e154693bf888657cd6426c3a
leekangtaqi <leekangtaqi@sina.com>
2016-05-03 15:03:21 +0800

    ok

--- src/modules/sd/order/services/OrderService.js
+++ src/modules/sd/order/services/OrderService.js
@@ -71,8 +71,6 @@ Service.prototype.getClearPriceAndUnclearPriceOfOrdersByOrdersAndDistributorId =
             var level = null;
             var price = null;
             var type = null;
-            console.log("order ***************");
-            console.log(order);
             if(order.closingDistributors && order.closingDistributors.indexOf(distributorId) >= 0){
                 level = order.closingDistributors.indexOf(distributorId) + 1;
                 type = 'clear';
@@ -80,20 +78,12 @@ Service.prototype.getClearPriceAndUnclearPriceOfOrdersByOrdersAndDistributorId =
                 level = order.distributors.indexOf(distributorId) + 1;
                 type = 'unclear';
             }
-            console.log("level ***************");
-            console.log(level);
             if(product['upLine' + level + 'CommissionType'] === 'c'){
                 price = parseFloat(product['upLine' + level + 'CommissionValue'], 10);
             }
             else{
-                console.log("$$$$$$$$$$$$$$$$$$$")
-                console.log(parseFloat(order.finalPrice, 10));
-                console.log("$$$$$$$$$$$$$$$$$$$")
-                console.log(parseFloat(product['upLine' + level + 'CommissionValue'], 10)/100);
                 price = parseFloat(order.finalPrice, 10) * (parseFloat(product['upLine' + level + 'CommissionValue'], 10)/100);
             }
-            console.log("price ***************");
-            console.log(price);
             return {
                 type: type,
                 price: price
