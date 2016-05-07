3f38b839c22b8b54932aa76598246a4670360dd7
node-rookie <749924911@qq.com>
2016-05-07 16:37:47 +0800

    remove routes wechat-platform

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ module.exports = function(app){
     require('./static')(app);
     app.keys = ['keys', 'keykeys'];
     app.use(require('../middlewares/session')());
-    require('./wechat-platform')(app);
+    //require('./wechat-platform')(app);
     require('./auth-wechat')(app);
     require('./auth')(app);
     require('../controllers')(app);
