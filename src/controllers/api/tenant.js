333825011d86fe1894fce96e7301e2df5a1ba64b
leekangtaqi <leekangtaqi@sina.com>
2016-05-07 16:48:00 +0800

    debug

--- src/controllers/api/tenant.js
+++ src/controllers/api/tenant.js
@@ -233,6 +233,8 @@ module.exports = function (router) {
     router.post('/sd/bespeak', function*(){
         try{
             let bespeak = this.request.body;
+            console.log("media***************");
+            console.log(bespeak.media);
             let wechatsite = yield context.services.tenantWechatSiteService.loadByIdAsync(bespeak.media);
             yield context.services.membershipService.ensureSignUpAsync(wechatsite.originalId, bespeak.user._id);
             yield context.services.bespeakService.createAsync({
