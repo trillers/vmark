5d6169b254fe04509ee5b1b65358b11f1afc55f5
node-rookie <749924911@qq.com>
2016-05-30 16:35:14 +0800

    try solve weixin forbidden

--- src/controllers/modules/power.js
+++ src/controllers/modules/power.js
@@ -51,41 +51,41 @@ module.exports = function(){
 
     router.get('/participant', needBaseInfoFilter, function *(){
         var id = this.query.id;
-        var user = this.session.auth && this.session.auth.user;
-        if(user) {
-            var participant = yield powerParticipantService.loadById(id);
-            if(participant) {
-                if(!participant.phone){
-                    this.redirect('/marketing/power/fullInfo?id=' + participant._id);
-                }else {
-                    if (participant.activity.lFlg === 'a') {
-                        var time = new Date().getTime();
-                        this.redirect('/marketing/power/' + time + '/participant?id=' + participant._id);
-                        //var status = yield powerParticipantService.getStatus(participant, user);
-                        //util.extend(participant, status);
-                        //var participants = yield powerActivityService.getParticipantRankingList(participant.activity._id, 200);
-                        //yield powerActivityService.increaseViews(participant.activity._id);
-                        //if (participant.activity.type === PowerType.RedPacket.value()) {
-                        //    yield this.render('/marketing/power/participant-redpacket', {
-                        //        participant: participant,
-                        //        participants: participants
-                        //    });
-                        //} else if (participant.activity.type === PowerType.Points.value()) {
-                        //    yield this.render('/marketing/power/participant-points', {
-                        //        participant: participant,
-                        //        participants: participants
-                        //    });
-                        //}
-                    } else {
-                        yield this.render('/marketing/power/error', {error: '活动暂未开放'});
-                    }
-                }
-            }else{
-                yield this.render('/marketing/power/error', {error: '页面不存在'});
-            }
-        }else{
-            yield this.render('/marketing/power/error', {error: '请用微信浏览器打开该页面'});
-        }
+        var time = new Date().getTime();
+        this.redirect('/marketing/power/' + time + '/participant?id=' + id);
+        //var user = this.session.auth && this.session.auth.user;
+        //if(user) {
+        //    var participant = yield powerParticipantService.loadById(id);
+        //    if(participant) {
+        //        if(!participant.phone){
+        //            this.redirect('/marketing/power/fullInfo?id=' + participant._id);
+        //        }else {
+        //            if (participant.activity.lFlg === 'a') {
+        //                var status = yield powerParticipantService.getStatus(participant, user);
+        //                util.extend(participant, status);
+        //                var participants = yield powerActivityService.getParticipantRankingList(participant.activity._id, 200);
+        //                yield powerActivityService.increaseViews(participant.activity._id);
+        //                if (participant.activity.type === PowerType.RedPacket.value()) {
+        //                    yield this.render('/marketing/power/participant-redpacket', {
+        //                        participant: participant,
+        //                        participants: participants
+        //                    });
+        //                } else if (participant.activity.type === PowerType.Points.value()) {
+        //                    yield this.render('/marketing/power/participant-points', {
+        //                        participant: participant,
+        //                        participants: participants
+        //                    });
+        //                }
+        //            } else {
+        //                yield this.render('/marketing/power/error', {error: '活动暂未开放'});
+        //            }
+        //        }
+        //    }else{
+        //        yield this.render('/marketing/power/error', {error: '页面不存在'});
+        //    }
+        //}else{
+        //    yield this.render('/marketing/power/error', {error: '请用微信浏览器打开该页面'});
+        //}
     });
 
     router.get('/:time/participant', needBaseInfoFilter, function *(){
